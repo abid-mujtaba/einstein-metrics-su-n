897957b6e729bba4b6ccb408b3c8b6d737205c31
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2020-12-24 14:05:01 -0500

    Add utility to check if an object is in an expression

---
+++ utilities.py
@@ -0,0 +1,22 @@
+"""Common utilities for manipulating or analyzing symbolic expressions."""
+
+from sympy import Expr
+from sympy.core.basic import preorder_traversal
+
+
+def is_in_expr(obj: Expr, expr: Expr):
+    """
+    Determine whether the obj is inside the expr.
+
+    If the obj is NOT a leaf (i.e. it has args, it combines other expressions) then
+    raise a ValueError.
+    """
+    if obj.args:
+        raise ValueError(f"obj {obj} is not a leaf, it has .args")
+
+    for e in preorder_traversal(expr):
+        if not e.args and e == obj:
+            return True
+
+    return False
+
