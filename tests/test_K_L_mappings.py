d1584046b27df29c880fc718d773e5bb2b7871ca
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2020-12-23 21:26:04 -0500

    Add initial test for creation of P matrix

--- tests/test_K_L_mappings.py
+++ tests/test_K_L_mappings.py
@@ -1,6 +1,7 @@
 """Test the K to L mappings defined in K_L_mappings.py."""
 
-from sympy import I
+from sympy import I, Rational
+from sympy.matrices import Matrix
 
 import K_L_mappings as sut
 
@@ -37,3 +38,18 @@ def test_category_2_K2L():
 
     assert K2L[0] == I * (L(0,1) - L(1,0))
     assert K2L[-1] == I * (L(1,2) - L(2,1))
+
+
+def test_P_matrix():
+    """Test the creation of the P matrix used to create the category 3 mappings."""
+    # GIVEN
+    n = 3
+
+    # WHEN
+    P = sut.create_P_matrix(n)
+
+    # THEN
+    assert isinstance(P, Matrix)
+    assert P.shape == (n, n)
+    assert P[0, 1] == Rational(2, n - 1)
+    assert float(P[0, 1]) == 1.0
