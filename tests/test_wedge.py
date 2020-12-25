95863470f5a00c6a20d3703214a5a2e2c79ad715
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2020-12-25 17:37:07 -0500

    Return 0 for wedge of an element with itself

--- tests/test_wedge.py
+++ tests/test_wedge.py
@@ -16,3 +16,15 @@ def test_representation():
 
     # THEN
     assert str(wedge) == "K₁ ∧ K₂"
+
+
+def test_zero_wedge_of_equal_1_forms():
+    """Taking the wedge of a 1-form with itself results in 0."""
+    # GIVEN
+    k_1 = K(1)
+
+    # WHEN
+    wedge = sut.Wedge(k_1, k_1)
+
+    # THEN
+    assert wedge == 0
--- wedge.py
+++ wedge.py
@@ -9,6 +9,17 @@ class Wedge(Expr):
     Focuses on just 2-forms with automatic ordering to ensure anti-symmetric
     cancellation by default.
     """
+    def __new__(cls, *args):
+        assert len(args) == 2
+        op1, op2 = args
+
+        if op1 == op2:  # Wedge product of an element with itself is zero by definition
+            return 0
+
+        obj = Expr.__new__(cls, *args)
+
+        return obj
+
     def _sympystr(self, printer, **kwargs):
         """Custom printer for the Wedge operation."""
         return f"{self.args[0]} ∧ {self.args[1]}"
