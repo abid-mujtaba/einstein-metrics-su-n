b4feed8ffbb7ab7b8b34ee3c5ea5581fafcb09d3
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2020-12-25 20:20:04 -0500

    Add function for differentiating linear sums of L 1-forms

--- differentials.py
+++ differentials.py
@@ -1,5 +1,6 @@
 """Calculate the differential (d<.>) of the L and K 1-forms."""
 
+from itertools import product
 from sympy import I, Expr
 from typing import List
 
@@ -16,6 +17,29 @@ def dL(a: int, b: int, n: int) -> Expr:
     where c is begin summed over (Einstein notation).
     """
     return I * sum(Wedge(L(a, i), L(i, b)) for i in range(n))
+
+
+def _differentiate_sum_of_L_1_forms(n: int, expr) -> Expr:
+    """Calculate the differential of an expression which is the sum of L 1-forms."""
+    # We will directly replace each L 1-form with its corresponding dL value to
+    # simulate the differential
+    # We are assuming that the expression is a linear combination of the L 1-forms
+    # with constant coefficients (whose differential is zero)
+    def sub_L(l: L) -> Expr:
+        a = l.index_1
+        b = l.index_2
+
+        return dL(a, b, n)
+
+    def sub(e: Expr) -> Expr:
+        """Recursive function for replacing L with dL."""
+        if isinstance(e, L):
+            return sub_L(e)
+
+        e._args = tuple(sub(arg) for arg in e.args)
+        return e
+
+    return sub(expr)
 
 
 def create_dK(n: int) -> List[Expr]:
--- tests/test_differentials.py
+++ tests/test_differentials.py
@@ -1,6 +1,6 @@
 """Test the differentials module."""
 
-from sympy import I
+from sympy import I, expand
 
 import differentials as sut
 
@@ -32,3 +32,34 @@ def test_create_dK():
 
     # THEN
     assert len(dK) == n**2
+
+
+def test_differentiate_sum_of_L_1_forms_single():
+    """Test the differentiation function using a single L 1-form."""
+    # GIVEN
+    n = 3
+    a = 1
+    b = 2
+    expr = L(a, b)
+
+    # WHEN
+    result = sut._differentiate_sum_of_L_1_forms(n, expr)
+
+    # THEN
+    assert result == sut.dL(a, b, n)
+
+
+def test_differential_sum_of_L_1_forms_double():
+    """Test the differentiation function using a linear combination of 2 L 1-forms."""
+    # GIVEN
+    n = 4
+    l_1 = L(1, 2)
+    l_2 = L(3, 0)
+
+    expr = 2 * l_1 - 3 * l_2
+
+    # WHEN
+    result = sut._differentiate_sum_of_L_1_forms(n, expr)
+
+    # THEN
+    assert expand(result) == expand(2 * sut.dL(1, 2, n) - 3 * sut.dL(3, 0, n))
