01f80baedc1add493ddf7cb253e755fceb02a5bc
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2020-12-28 10:03:23 -0500

    Add inverse metric to creation

--- metric.py
+++ metric.py
@@ -2,6 +2,7 @@
 
 from sympy import Array, Symbol
 from sympy.matrices import eye
+from typing import Tuple
 
 
 # Create the metric constants (one for each category for the K 1-forms)
@@ -10,20 +11,25 @@ x2 = Symbol("x₂")
 x3 = Symbol("x₃")
 
 
-def create_metric(n: int) -> Array:
-    """Create the metric tensor in the K 1-form space for SU(n)."""
+def create_metric(n: int) -> Tuple[Array, Array]:
+    """Create the metric tensor and its inverse in the K 1-form space for SU(n)."""
     dim = n**2 - 1  # Dimension of the space
     m = int(n * (n - 1) / 2)  # Size of categories 1 and 2
 
     # Start with a diagonal matrix
     e = eye(dim)
+    ei = eye(dim)
 
     for i in range(m):
         e[i, i] = x1
+        ei[i, i] = 1 / x1
+
         e[m + i, m + i] = x2
+        ei[m + i, m + i] = 1 / x2
 
     for i in range(n - 1):  # Category 3 K 1-forms are (n - 1) in number after you
                             # ignore the K_{n**2} 1-form which is NOT a part of SU(n)
         e[2 * m + i, 2 * m + i] = x3
+        ei[2 * m + i, 2 * m + i] = 1 / x3
 
-    return Array(e)
+    return Array(e), Array(ei)
--- tests/test_metric.py
+++ tests/test_metric.py
@@ -1,18 +1,26 @@
 """Test the metric module."""
 
+from sympy.tensor import tensorcontraction as tc, tensorproduct as tp
 import metric as sut
 
 
 def test_create_metric() -> None:
-    """Test the creation of the metric."""
+    """Test the creation of the metric, and its inverse."""
     # GIVEN
     n = 3
 
     # WHEN
-    g_dd = sut.create_metric(n)
+    g_dd, g_uu = sut.create_metric(n)
+
+    # Test inversion using tensor contraction
+    kron_ud = tc(tp(g_uu, g_dd), (1, 2))
+
+    # Contraction of the kronecker delta
+    scalar = tc(kron_ud, (0,1))
 
     # THEN
     assert g_dd.shape == (8, 8)
+    assert g_uu.shape == (8, 8)
 
     assert g_dd[0,0] == sut.x1
     assert g_dd[3,3] == sut.x2
@@ -20,3 +28,20 @@ def test_create_metric() -> None:
 
     assert g_dd[0,1] == 0
     assert g_dd[1,0] == 0
+
+    assert g_uu[0,0] == 1 / sut.x1
+    assert g_uu[3,3] == 1 / sut.x2
+    assert g_uu[7,7] == 1 / sut.x3
+
+    assert g_uu[0,1] == 0
+    assert g_uu[1,0] == 0
+
+    assert kron_ud.shape == (8, 8)
+
+    for i in range(8):
+        assert kron_ud[i,i] == 1
+
+    assert kron_ud[0,1] == 0
+    assert kron_ud[1,0] == 0
+
+    assert scalar == 8
