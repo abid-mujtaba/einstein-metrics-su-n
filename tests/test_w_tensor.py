223d63fba543307600a1b91a32e2460bce4bff4c
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2020-12-29 18:12:11 -0500

    Add generic test for w_dd

--- tests/test_w_tensor.py
+++ tests/test_w_tensor.py
@@ -2,7 +2,9 @@
 
 import pytest
 
+from itertools import product
 from sympy import sqrt, Array, Expr, expand
+from sympy.tensor import permutedims as pd
 from typing import List
 
 from c_tensor import create_c_ddu, create_c_ddd
@@ -32,6 +34,30 @@ def c_ddd_fixture(n: int, dK: List[Expr]) -> Array:
 def dK_u_fixture(n: int) -> Array:
     """Create the dK_u tensor of the differentials of the K 1-forms."""
     return create_K_u(n)
+
+
+def _K_in_expr(n: int, expr: Expr) -> bool:
+    """Verify that there is a K 1-form in the expression."""
+    if isinstance(expr, K):
+        return 0 <= expr.index < n**2
+
+    if expr.args:
+        return any(_K_in_expr(n, arg) for arg in expr.args)
+
+    return False
+
+
+@pytest.mark.parametrize("n", (3,))
+def test_create_w_dd(n: int, c_ddd: Array, K_u: Array) -> None:
+    """General test of the creation of the w_dd tensor."""
+    # WHEN
+    w_dd = sut.create_w_dd(c_ddd, K_u)
+    antisymm_sum = w_dd + pd(w_dd, (1,0))
+
+    # THEN: Verify antisymmetry. Verify that the w_dd contain K 1-forms
+    for i, j in product(range(n**2 - 1), repeat=2):
+        assert antisymm_sum[i,j] == 0
+        assert w_dd[i,j] == 0 or _K_in_expr(n, w_dd[i,j])
 
 
 @pytest.mark.parametrize("n", (2,))
