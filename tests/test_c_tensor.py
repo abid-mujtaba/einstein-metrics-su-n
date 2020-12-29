8cf87c43071e42adf971add05a3c659056c80e78
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2020-12-28 22:34:23 -0500

    Add test for _coeff in c_tensor

--- tests/test_c_tensor.py
+++ tests/test_c_tensor.py
@@ -1,7 +1,9 @@
 """Test the c_tensor module."""
 
+from sympy import sqrt
 from sympy.abc import x, y
 
+from differentials import create_dK
 from K_1_forms import K
 from wedge import Wedge
 
@@ -93,3 +95,27 @@ def test_extract_coeff_non_matching_wedge_in_linear_combination() -> None:
 
     # THEN
     assert result == 0
+
+
+def test_coeff_n_equals_2() -> None:
+    """Test _coeff against hand-calculations for n-2."""
+    # GIVEN
+    n = 2
+    dK = create_dK(n)
+
+    # THEN
+    assert sut._coeff(dK, 0, 1, 2) == -1 * sqrt(2)
+    assert sut._coeff(dK, 1, 0, 2) == sqrt(2)
+    assert sut._coeff(dK, 2, 0, 1) == -1 / sqrt(2)
+
+    # ._coeff is designed to be anti-symmetric in b and c
+    assert sut._coeff(dK, 0, 2, 1) == sqrt(2)
+    assert sut._coeff(dK, 1, 2, 0) == -1 * sqrt(2)
+    assert sut._coeff(dK, 2, 1, 0) == 1 / sqrt(2)
+
+    # Everything else is zero
+    assert sut._coeff(dK, 0, 0, 0) == 0
+    assert sut._coeff(dK, 0, 0, 1) == 0
+    assert sut._coeff(dK, 0, 0, 2) == 0
+    assert sut._coeff(dK, 0, 1, 1) == 0
+    assert sut._coeff(dK, 0, 2, 2) == 0
