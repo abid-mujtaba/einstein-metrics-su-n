9ff8753b5e531c8b9d9b61e3e0c99e33adb22a24
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2020-12-31 10:06:28 -0500

    test: Add for test _is_*_in_expr moved to utilities

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -2,9 +2,12 @@
 
 import pytest
 
-from sympy.abc import x, y, z
+from sympy.abc import a, b, x, y, z
 
 import utilities as sut
+
+from K_1_forms import K
+from wedge import Wedge
 
 
 def test_is_in_expr_error_if_object_is_not_a_leaf() -> None:
@@ -18,7 +21,6 @@ def test_is_in_expr_error_if_object_is_not_a_leaf() -> None:
         sut.is_in_expr(obj, expr)
 
 
-# TODO: Parametrize this
 def test_is_in_expr_true() -> None:
     """Test when the object is in the expression."""
     # GIVEN
@@ -32,7 +34,6 @@ def test_is_in_expr_true() -> None:
     assert result is True
 
 
-# TODO: Parametrize this
 def test_is_in_expr_false() -> None:
     """Test when the object is NOT in the expression."""
     # GIVEN
@@ -41,6 +42,71 @@ def test_is_in_expr_false() -> None:
 
     # WHEN
     result = sut.is_in_expr(obj, expr)
+
+    # THEN
+    assert result is False
+
+
+def test_is_K_in_expr_true() -> None:
+    """Test the utility on a linear combination of K 1-forms."""
+    # GIVEN
+    n = 2
+    expr = a * K(0) + b * K(0)
+
+    # WHEN
+    result = sut.is_K_in_expr(n, expr)
+
+    # THEN
+    assert result is True
+
+
+def test_is_K_in_expr_false() -> None:
+    """Test the utility on a linear combination without K 1-forms."""
+    # GIVEN
+    n = 2
+    expr = a * x + b * y
+
+    # WHEN
+    result = sut.is_K_in_expr(n, expr)
+
+    # THEN
+    assert result is False
+
+
+def test_is_Wedge_in_expr_True() -> None:
+    """Test the utility on a linear combination of wedges."""
+    # GIVEN
+    n = 2
+    expr = a * Wedge(K(0), K(1)) + b * Wedge(K(1), K(2))
+
+    # WHEN
+    result = sut.is_Wedge_of_K_in_expr(n, expr)
+
+    # THEN
+    assert result is True
+
+
+def test_is_Wedge_of_K_in_expr_no_wedge_false() -> None:
+    """Test the utility on a linear combination without wedges."""
+    # GIVEN
+    n = 2
+    expr = a * x + b * y
+
+    # WHEN
+    result = sut.is_Wedge_of_K_in_expr(n, expr)
+
+    # THEN
+    assert result is False
+
+
+def test_is_Wedge_of_K_in_expr_no_K_wedge_false() -> None:
+    """Test the utility on a Wedge which is NOT over K 1-forms."""
+    # GIVEN
+    n = 2
+    expr = a * Wedge(x, y) + b * Wedge(y, z)
+
+    # WHEN
+    result = sut.is_Wedge_of_K_in_expr(n, expr)
 
     # THEN
     assert result is False
