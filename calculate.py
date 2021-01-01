ee5068b1c258eede3495f7a695db76aa1ddc8644
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2021-01-01 14:16:11 -0500

    Calculate the Ricci tensor for specified n

---
+++ calculate.py
@@ -0,0 +1,49 @@
+"""Carry out the full sequence of calculations to find Einstein Metrics."""
+
+import sys
+
+from sympy import Eq, Symbol, expand
+from sympy.solvers import nonlinsolve, solve
+
+from c_tensor import create_c_ddd, create_c_ddu
+from differentials import create_dK
+from K_1_forms import create_K_u
+from metric import create_metric, x1, x2, x3
+from ricci import create_R_uddd, create_R_dd
+from theta_tensor import create_theta_ud
+from w_tensor import create_w_dd, create_w_ud, create_w_wedge_ud, create_dw_ud
+
+# Choose the dimenions of the Group SU(n)
+n = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+
+dim = n ** 2 - 1
+m = int(n * (n - 1) / 2)
+
+dK = create_dK(n)
+
+g_dd, g_uu = create_metric(n)
+
+c_ddu = create_c_ddu(dK, n)
+c_ddd = create_c_ddd(c_ddu, g_dd)
+
+K_u = create_K_u(n)
+
+w_dd = create_w_dd(c_ddd, K_u)
+w_ud = create_w_ud(w_dd, g_uu)
+
+dw_ud = create_dw_ud(w_ud, dK)
+w_wedge_ud = create_w_wedge_ud(n, w_ud)
+theta_ud = create_theta_ud(dw_ud, w_wedge_ud)
+
+R_uddd = create_R_uddd(n, theta_ud)
+R_dd = create_R_dd(R_uddd)
+
+assert len(set(R_dd[i, i] for i in range(dim))) == 3
+
+e_00 = R_dd[0, 0]
+e_11 = R_dd[m, m]
+e_22 = R_dd[2*m, 2*m]
+
+print(e_00)
+print(e_11)
+print(e_22)
