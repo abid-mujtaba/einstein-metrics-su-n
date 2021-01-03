effe1268154fe159eaeabc3604275a4f81e90526
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2021-01-03 15:16:23 -0500

    feat: Pickle the Riemann tensor while calculating

--- calculate.py
+++ calculate.py
@@ -1,15 +1,13 @@
 """Carry out the full sequence of calculations to find Einstein Metrics."""
 
+import dill
 import sys
-
-from sympy import Eq, Symbol, expand
-from sympy.solvers import nonlinsolve, solve
 
 from c_tensor import create_c_ddd, create_c_ddu
 from differentials import create_dK
 from K_1_forms import create_K_u
 from metric import create_metric, x1, x2, x3
-from ricci import create_R_uddd, create_R_dd
+from ricci import create_R_uddd, create_R_dd, calculate_Riem_2
 from theta_tensor import create_theta_ud
 from w_tensor import create_w_dd, create_w_ud, create_w_wedge_ud, create_dw_ud
 
@@ -44,6 +42,13 @@ e_00 = R_dd[0, 0]
 e_11 = R_dd[m, m]
 e_22 = R_dd[2 * m, 2 * m]
 
+print(f"Calculation for n = {n}\n")
+print("Unique elements of Ricci tensor:\n")
 print(e_00)
 print(e_11)
 print(e_22)
+
+# Serialize and save the R_uddd tensor
+dill.settings["recurse"] = True
+with open(f"R_uddd_n_{n}.dat", "wb") as fout:
+    dill.dump(R_uddd, fout)
